22f3ab14101c7bf9aedd6bd7e8b3c01ae81d4d09
nathanpeter636 <nathanpeter636@gmail.com>
2021-05-07 11:15:28 +1200

    init

---
+++ config.js
@@ -0,0 +1,8 @@
+
+
+module.exports = {
+    MONGODB:
+      'mongodb+srv://nathanpeter636:secret33@cluster0.zpgfz.mongodb.net/merng?retryWrites=true&w=majority',
+
+      SECRET_KEY: 'VERY SECRET KEY'
+  };
